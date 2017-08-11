0729321dff7da5dd1a86b1a22706588d91a3f9c3
alexMontague <xturnermonty@gmail.com>
2017-08-10 23:08:54 -0400

    Perl Data Manipulation Script

---
+++ dataManipulation.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+ 
+my $filename = "test.txt";
+
+open(my $fh, '<:encoding(UTF-8)', $filename)
+	or die "Could not open file '$filename' $!";
+ 
+while (my $row = <$fh>) {
+	chomp $row;
+	my $rowCopy = substr($row, 0, 9);		# Search by just the course code not including the description
+	if($rowCopy =~ /(^[A-Z])*\d\d\d\d/) {	# Regular Expression filtering out the other lines
+		print "$row\n";						# Printing to the terminal, used > 'file.txt' to get a file
+	}
+}
+
+print "done\n";
