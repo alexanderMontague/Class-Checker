eab096ffbe039438a100c196e9f0b46f39453e4e
alexMontague <xturnermonty@gmail.com>
2017-08-13 00:11:14 -0400

    Main Program File - Added class import

--- ClassChecker.c
+++ ClassChecker.c
@@ -10,11 +10,23 @@ void removeNewline(char inputString[50]);
 // Main
 int main(void)
 {
+	// Struct Decleration
+	struct classInfo {
+		char classCode[50];
+		char classType[50];
+		char classLoc[50];
+	};
 
 	// Variable Decleration
+	int numClasses = 1;
+	int lineChecker = 1;
+
 	char menuInput[100];
 	char enterCode[100];
 	char importMenu[100];
+	char tempInput[100];
+
+	struct classInfo classArray[numClasses];
 
 	FILE *timetableDataFP = NULL;
 
@@ -37,7 +49,10 @@ int main(void)
 
 		if(strcmp(menuInput, "Import") == 0) {
 
-			timetableDataFP = fopen("timetableData.txt", "w");
+			timetableDataFP = fopen("timetableData.txt", "r");
+			if(timetableDataFP == NULL) {
+				timetableDataFP = fopen("timetableData.txt", "w");
+			}
 			fclose(timetableDataFP);
 
 			printf("\nCopy and Paste your timetable data into the text file 'timetableData.txt' in this directory!\n");
@@ -52,19 +67,38 @@ int main(void)
 				if(strcmp(importMenu, "Help") == 0) {
 					printf("\nNavigate to the Class Schedule Grid screen that can be found on webadvisor.\n");
 					printf("Click and drag from the 'Class Schedule Grid' title text, all the way down to the end of your name.\n");
-					printf("Copy the selected text (ctrl/cmmnd + C OR right click and select'copy'.)\n");
+					printf("Copy the selected text (ctrl/cmd + C OR right click and select 'copy'.)\n");
 					printf("Navigate to the folder this program is contained in.\n");
 					printf("Open the file 'timetableData.txt'\n");
 					printf("Paste the contents into the empty file.\n");
-					printf("Save the file (ctrl/cmmnd + S OR click file > save)\n");
-					printf("That is it!\n");
-					printf("If you would like to see a graphical representation, open the file 'timetableHelp'!\n\n");
+					printf("Save the file (ctrl/cmd + S OR click file > save)\n");
+					printf("That's it!\n");
+					printf("If you would like to see a graphical representation, open the file 'timetableHelp' in the directory!\n\n");
 
 				}	
 				else if(strcmp(importMenu, "Ready") == 0) {
-					
-					
-					
+										
+					timetableDataFP = fopen("timetableData.txt", "r");
+					fseek(timetableDataFP, 21, SEEK_SET);	// Seeking to where the class codes start. Will be the same for every paste.
+															// SOMETHING WRONG HERE, Second line (first entry location) not being read or something
+					while(strcmp(tempInput, " ") != 0) {
+						while(lineChecker < 4) {
+							fgets(tempInput, sizeof(tempInput), timetableDataFP);
+							removeNewline(tempInput);
+							if(lineChecker == 1) {
+								strcpy(classArray[numClasses].classCode, tempInput);
+							}
+							else if(lineChecker == 2) {
+								strcpy(classArray[numClasses].classType, tempInput);
+							}
+							else if(lineChecker == 3) {
+								strcpy(classArray[numClasses].classLoc, tempInput);
+							}
+							lineChecker++;
+						}
+						printf("Class: %s Type: %s Loc: %s\n", classArray[numClasses].classCode, classArray[numClasses].classType, classArray[numClasses].classLoc);						lineChecker = 1;
+						numClasses++;
+					}
 					loopMenu2 = false;
 				}
 				else {
@@ -85,6 +119,7 @@ int main(void)
 			printf("Enter friend's code: ");
 			fgets(enterCode, sizeof(enterCode), stdin);
 			removeNewline(enterCode);
+			printf("\n");
 
 			// Process code and then tell if there are classes together
 		}
